a00b8dfc3c942f20e7a1e7b36beccb80f796729b
Renan Rodrigo <renanrb@ime.usp.br>
2018-05-02 21:51:52 -0300

    Mudar o comportamento para sortear uma cor aleatoria

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 let quadrados = document.querySelectorAll(".interna");
 
+let cores = ["blue","red","yellow","green"]
+
+function aleatorio(min, max){
+  return Math.floor(Math.random() * (max-min+1)) + min;
+}
+
 function trocarCor(){
-  this.style.backgroundColor = "blue";
+  let cor = aleatorio(0,3);
+  this.style.backgroundColor = cores[cor];
 }
 
 for(let quadrado of quadrados){
